a5d718480d87b66091a4dc51cc987e49bb047abf
Rocio Nerea Martinez Subidia <rociomartinezsubidia@gmail.com>
2025-09-06 22:06:22 -0300

    Arreglo errores

--- web/__init__.py
+++ web/__init__.py
@@ -3,10 +3,10 @@ from flask import Flask
 def crear_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'Labo2025'
-    from.rutas import registrar_rutas, vistas
-    from.empleado import empleado
-    from.admin import admin
-    from.visualizar_dataset import generar_graficos
+    from .rutas import registrar_rutas, vistas
+    from .empleado import empleado
+    from .admin import admin
+    from .visualizar_dataset import generar_graficos
 
     app.register_blueprint(vistas, url_prefix='/')
     app.register_blueprint(empleado, url_prefix='/empleado')
